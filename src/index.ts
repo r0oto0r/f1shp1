150aca00aaec0d63054c0cdb22c829c22d6a35cb
Ben <ben@forkbomb.dev>
2021-09-08 21:27:28 +0100

    Changed to node 10.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import express, { Application, Request, Response } from "express";
+import { LedMatrix } from 'rpi-led-matrix';
+ 
+const matrix = new LedMatrix(
+  LedMatrix.defaultMatrixOptions(),
+  LedMatrix.defaultRuntimeOptions()
+);
 
 const app: Application = express();
 const port = 3000;
@@ -8,7 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", async (req: Request, res: Response): Promise<Response> => {
-        return res.status(200).send({
+	console.log(req.ip, req.url);
+
+	return res.status(200).send({
             message: "Hello World!",
         });
     }
